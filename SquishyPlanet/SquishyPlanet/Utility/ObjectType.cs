e1252c95b0986483a2267fdc1a5fc72041ac2709
Michael <michaelschmidtnissen@gmail.com>
2025-11-10 16:58:34 +0100

    evening commit

---
+++ SquishyPlanet/SquishyPlanet/Utility/ObjectType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquishyPlanet.Utility
+{
+    public enum ObjectType : byte
+    {
+        Particle = 1,
+        Wheel = 2,
+        Constraint = 4,
+        FixedConstraint = 8,
+        FluidParticle = 16,
+        SoftBody = 32,
+        FixedConstraintParticle = 64
+    }
+}
